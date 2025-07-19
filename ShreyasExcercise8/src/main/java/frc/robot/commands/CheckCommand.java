ee5c5b71d1314bb1923a7daab841bd2a561fb478
Sudhamsu <academyoffice79@gmail.com>
2025-07-19 16:36:44 -0400

    exercise 8 completed

---
+++ ShreyasExcercise8/src/main/java/frc/robot/commands/CheckCommand.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.UltrasonicSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class CheckCommand extends Command {
+  /** Creates a new CheckCommand. */
+  public CheckCommand() {
+    addRequirements(RobotContainer.ultrasonicSubsystem);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+      if(UltrasonicSubsystem.getDistance() > 500){
+      System.out.println("Goody");
+      RobotContainer.canDrive = true;
+      } else {
+      System.out.println("Not Goody");
+      RobotContainer.canDrive = false;
+      }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

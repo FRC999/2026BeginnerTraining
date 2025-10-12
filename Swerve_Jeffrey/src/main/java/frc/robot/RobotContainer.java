7dec8ca4db0ce32adff2cb9598d33104f38185c9
Sudhamsu <academyoffice79@gmail.com>
2025-10-12 15:59:22 -0400

    attempted swerve command implementation

--- Swerve_Jeffrey/src/main/java/frc/robot/RobotContainer.java
+++ Swerve_Jeffrey/src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,9 @@ import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+
+import java.lang.ModuleLayer.Controller;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -29,11 +32,22 @@ public class RobotContainer {
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
+  // Creating an xbox controller for swerveDriveTrain
+  public static Controller xboxController;
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
+    // Put the driveManuallyCommand, which should be default command, allowing the xbox controller
+
   }
+
+  // private double getDriverXAxis() {
+  //   return -xboxController.getLeftStickY();
+  // }
+
 
   /**
    * Use this method to define your trigger->command mappings. Triggers can be created via the

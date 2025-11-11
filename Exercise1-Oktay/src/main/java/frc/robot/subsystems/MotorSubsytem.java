e0abd831174ed0921d197f3913ea149617ff8ced
Sudhamsu <academyoffice79@gmail.com>
2025-11-10 20:50:01 -0500

    Exercise 2 - midway done - Oktay

--- Exercise1-Oktay/src/main/java/frc/robot/subsystems/MotorSubsytem.java
+++ Exercise1-Oktay/src/main/java/frc/robot/subsystems/MotorSubsytem.java
@@ -5,13 +5,17 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.revrobotics.jni.CANSparkJNI;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class MotorSubsytem extends SubsystemBase {
   
   WPI_TalonSRX motor1 = new WPI_TalonSRX(9);
-  
+  SparkMax motor2 = new SparkMax(54, MotorType.kBrushless);
+}
   /** Creates a new MotorSubsytem. */
   public MotorSubsytem() {
   

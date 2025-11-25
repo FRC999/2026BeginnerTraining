1285bdca477512a0eb82f26528059dfe57060ef0
Sudhamsu <academyoffice79@gmail.com>
2025-11-24 20:50:19 -0500

    Oktay - Completed exercise 3

--- Oktay-Exercise 3/src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ Oktay-Exercise 3/src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -5,9 +5,11 @@
 package frc.robot.subsystems;
 
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.jni.CANSparkJNI;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
@@ -20,18 +22,31 @@ public class MotorSubsystem extends SubsystemBase {
   SparkMax leftMotor = new SparkMax(57, MotorType.kBrushless) ;
   SparkMax rightMotor = new SparkMax(54, MotorType.kBrushless);
 
+  private SparkMaxConfig leftConfig = new SparkMaxConfig();
+  private SparkMaxConfig rightConfig = new SparkMaxConfig();
+
+  private DifferentialDrive chasis;
+  
   
   /** Creates a new MotorSubsystem. */
   public MotorSubsystem() {
 
-    
-    motorConfig = new SparkMaxConfig();   
-    
-   
+    leftConfig.inverted(false);
+
+    rightConfig.inverted(true);
+
+    leftMotor.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    rightMotor.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+    chasis = new DifferentialDrive(leftMotor, rightMotor);
 
   }
 
- 
+  public void drive(double x , double y) {
+    chasis.arcadeDrive(x, y);
+
+  }
+  
   
 
 

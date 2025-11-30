b8c72bf7b1f491b79d5bd93757c7fdb5f03b51f4
Sudhamsu <academyoffice79@gmail.com>
2025-11-30 17:42:47 -0500

    Exercise 3 not completed - code not starting?

--- Exercise_2_Jonah/src/main/java/frc/robot/subsystems/MotorSubsystem.java
+++ Exercise_2_Jonah/src/main/java/frc/robot/subsystems/MotorSubsystem.java
@@ -9,7 +9,6 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
 import edu.wpi.first.wpilibj.Joystick;

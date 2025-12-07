0d609f2e03b1419fe1900a940785d920de8e2c5e
Sudhamsu <academyoffice79@gmail.com>
2025-12-07 15:30:25 -0500

    Finished IMU's including Pigeon 1 and navX - Oktay

--- IMU_Pigeon 1-Oktay/src/main/java/frc/robot/subsystems/IMUsubsystem.java
+++ IMU_Pigeon 1-Oktay/src/main/java/frc/robot/subsystems/IMUsubsystem.java
@@ -7,6 +7,9 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
+import com.studica.frc.AHRS.NavXComType;
+import com.studica.frc.AHRS;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -14,6 +17,8 @@ public class IMUsubsystem extends SubsystemBase {
   /** Creates a new IMUsubsystem. */
   private final TalonSRX talon = new TalonSRX(4);
   private final PigeonIMU pigeon = new PigeonIMU(talon);
+
+  AHRS navX = new AHRS(NavXComType.kMXP_SPI);
 
   public IMUsubsystem() {
 
@@ -26,9 +31,14 @@ public class IMUsubsystem extends SubsystemBase {
     pigeon.getYawPitchRoll(ypr);
 
     System.out.println("Yaw: %.2f  Pitch: %.2f  Roll: %.2f%n" + ypr[0] + " " + ypr[1] + " " + ypr[2]);
-    SmartDashboard.putNumber("Yaw", ypr[0]);
-    SmartDashboard.putNumber("Pitch", ypr[1]);
-    SmartDashboard.putNumber("Roll", ypr[2]);
+    
+    SmartDashboard.putNumber("Pigeon Yaw", ypr[0]);
+    SmartDashboard.putNumber("Pigeon Pitch", ypr[1]);
+    SmartDashboard.putNumber("Pigeon Roll", ypr[2]);
+
+    SmartDashboard.putNumber("navX Yaw", navX.getYaw());
+    SmartDashboard.putNumber("navX Pitch", navX.getRoll());
+    SmartDashboard.putNumber("navX Roll", navX.getPitch());
 
   }
 }

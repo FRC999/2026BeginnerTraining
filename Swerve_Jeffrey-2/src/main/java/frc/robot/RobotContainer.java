7094314c5027070ba065479d35edaf4d7d9ce534
Sudhamsu <academyoffice79@gmail.com>
2025-12-29 18:47:35 -0500

    fixed swerve code

---
+++ Swerve_Jeffrey-2/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,107 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.DriveManuallyCommand;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ExampleSubsystem;
+
+import java.lang.ModuleLayer.Controller;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
+  public static final DriveSubsystem driveSubsystem = DriveSubsystem.driveTrain();  
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  // Creating an xbox controller for swerveDriveTrain
+  public static Controller xboxDriveController;
+
+  public static boolean isAllianceRed = false;
+
+
+  public RobotContainer() {
+    configureBindings();
+    driveSubsystem.setDefaultCommand(
+      new DriveManuallyCommand(
+          () -> getDriverXAxis(),
+          () -> getDriverYAxis(),
+          () -> getDriverOmegaAxis()));
+  }
+
+
+
+  public void checkIfAllianceIsRed() {
+    var alliance = DriverStation.getAlliance();
+    isAllianceRed = alliance.get() == DriverStation.Alliance.Red;
+  }
+
+     // Driver preferred controls
+     private double getDriverXAxis() {
+      return -m_driverController.getLeftY();
+      //return -xboxDriveController.getRightStickY();
+    }
+  
+    private double getDriverYAxis() {
+      return -m_driverController.getLeftX();
+      //return -xboxDriveController.getRightStickX();
+    }
+  
+    private double getDriverOmegaAxis() {
+      //return -xboxController.getLeftStickOmega();
+      //return -xboxDriveController.getLeftStickX();
+      return -m_driverController.getRightX();
+    }
+
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
+}
---
+++ Swerve_Jeffrey-2/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,102 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Rotations;
+
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.swerve.SwerveDrivetrain;
+import com.ctre.phoenix6.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentric;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Constants.SwerveConstants;
+
+public class DriveSubsystem extends SwerveDrivetrain<TalonFX, TalonFX, CANcoder> implements Subsystem {
+  /** Creates a new DriveSubsystem. */
+  Pigeon2 imu;
+
+    private final FieldCentric drive = new FieldCentric()
+            .withDeadband(SwerveConstants.MaxSpeed * SwerveConstants.DeadbandRatioLinear)
+            .withRotationalDeadband(SwerveConstants.MaxAngularRate * SwerveConstants.DeadbandRatioAngular)
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Will not check if got to location
+
+  public static DriveSubsystem driveTrain() {
+    return new DriveSubsystem(
+      SwerveConstants.DrivetrainConstants,
+      SwerveConstants.ConstantCreator.createModuleConstants(
+        SwerveConstants.MOD0.angleMotorID(), 
+        SwerveConstants.MOD0.driveMotorID(),
+        SwerveConstants.MOD0.cancoderID(), 
+        Rotations.of(SwerveConstants.MOD0.angleOffset()), 
+        Inches.of(10.335), Inches.of(10.335), 
+        SwerveConstants.MOD0.driveMotorInverted(),
+        SwerveConstants.MOD0.angleMotorInverted(),
+        SwerveConstants.MOD0.cancoderInverted()),
+      SwerveConstants.ConstantCreator.createModuleConstants(
+        SwerveConstants.MOD1.angleMotorID(), 
+        SwerveConstants.MOD1.driveMotorID(),
+        SwerveConstants.MOD1.cancoderID(), 
+        Rotations.of(SwerveConstants.MOD1.angleOffset()), 
+        Inches.of(10.335), Inches.of(-10.335), 
+        SwerveConstants.MOD1.driveMotorInverted(),
+        SwerveConstants.MOD1.angleMotorInverted(),
+        SwerveConstants.MOD1.cancoderInverted()),
+      SwerveConstants.ConstantCreator.createModuleConstants(
+        SwerveConstants.MOD2.angleMotorID(), 
+        SwerveConstants.MOD2.driveMotorID(),
+        SwerveConstants.MOD2.cancoderID(), 
+        Rotations.of(SwerveConstants.MOD2.angleOffset()), 
+        Inches.of(-10.335), Inches.of(10.335), 
+        SwerveConstants.MOD2.driveMotorInverted(),
+        SwerveConstants.MOD2.angleMotorInverted(),
+        SwerveConstants.MOD2.cancoderInverted()), 
+      SwerveConstants.ConstantCreator.createModuleConstants(
+        SwerveConstants.MOD3.angleMotorID(), 
+        SwerveConstants.MOD3.driveMotorID(),
+        SwerveConstants.MOD3.cancoderID(), 
+        Rotations.of(SwerveConstants.MOD3.angleOffset()), 
+        Inches.of(-10.335), Inches.of(-10.335), 
+        SwerveConstants.MOD3.driveMotorInverted(),
+        SwerveConstants.MOD3.angleMotorInverted(),
+        SwerveConstants.MOD3.cancoderInverted()));
+  }
+
+  public DriveSubsystem( 
+            SwerveDrivetrainConstants drivetrainConstants,
+            SwerveModuleConstants<?, ?, ?>... modules) {
+    super( 
+        TalonFX::new, TalonFX::new, CANcoder::new,
+        drivetrainConstants, modules);
+    CommandScheduler.getInstance().registerSubsystem(this);
+    imu = this.getPigeon2();
+  }
+
+  // Completely no idea what this does, someone pls help review
+  public void drive(double xVelocity_m_per_s, double yVelocity_m_per_s, double omega_rad_per_s) {
+    //System.out.println("X: " + xVelocity_m_per_s + " y: " + yVelocity_m_per_s + " o:" + omega_rad_per_s/SwerveChassis.MaxAngularRate);
+    SmartDashboard.putString("Manual Drive Command Velocities","X: " + xVelocity_m_per_s + " y: " + yVelocity_m_per_s + " o:" + omega_rad_per_s);
+    this.setControl(
+      drive.withVelocityX(xVelocity_m_per_s)
+        .withVelocityY(yVelocity_m_per_s)
+        .withRotationalRate(omega_rad_per_s)
+    );
+    // previousOmegaRotationCommand = omega_rad_per_s / SwerveChassis.MaxAngularRate;
+  }
+  
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
